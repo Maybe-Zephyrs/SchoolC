9b495e5cb42bc0863b62b172b6c4807c4625d59c
Maybe-Zephyrs <3103876127@qq.com>
2026-04-23 12:44:45 +0800

    git

--- S6/t1/Exp5_1.cpp
+++ S6/t1/Exp5_1.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 void main()
 {
-    int a,b,t;
-    scanf("%d%d",&a,&b);
-    printf("a=%db=%d\n",a,b);
-    t=a;a=b;b=t;
-    printf("a=%db=%d\n",a,b);
+    int a, b, t;
+    scanf("%d%d", &a, &b);
+    printf("a=%db=%d\n", a, b);
+    t = a;
+    a = b;
+    b = t;
+    printf("a=%db=%d\n", a, b);
 }
